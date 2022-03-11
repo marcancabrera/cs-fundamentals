104d921e991994c1c23fc264c764bdacdb978d8c
Marcandea Cabrera Quispe <Marcandea.Cabrera@jalasoft.com>
2022-03-11 14:38:28 -0400

    Start request Manager

---
+++ API/HttpRequestManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AutomationAPI.API
+{
+    internal class HttpRequestManager
+    {
+        private string url = "http://localhost:9093";
+
+        public async Task<HttpResponseMessage> MakeRequest(string verb, string endpoint, HttpContent? data = null)
+        {
+            HttpClient client = new HttpClient();
+            url = $"{url}/{endpoint}";
+            HttpResponseMessage response;
+            switch (verb)
+            {
+                case "GET":
+                    response = await client.GetAsync(url);
+                    break;
+                case "POST":
+                    response = await client.PostAsync(url, data);
+                    break;
+                case "PUT":
+                    response = await client.PutAsync(url, data);
+                    break;
+                case "DELETE":
+                    response = await client.DeleteAsync(url);
+                    break;
+                default:
+                    throw new Exception("Verb doesn't exist");
+            }
+            return response;
+        }
+    }
+}
